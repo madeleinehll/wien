da2260dd803bce83f0bcdaa163d8ef8ba895406b
madeleinehll <hill.madeleine13@yahoo.de>
2024-04-18 17:39:19 +0200

    Pop up designen, mit Name, Bild, Link und Adresse

--- main.js
+++ main.js
@@ -54,11 +54,24 @@ map.addControl(new L.Control.Fullscreen());
 //.fullscreen ()
 //.addTo(map);
 
+//* für Schmuckbild
+//target = öffnen in neuem Tab
 async function loadSights(url) {
   console.log("Loading", url);
   let response = await fetch (url);
   let geojson = await response.json();
   //console.log(geojson);
-  L.geoJSON(geojson).addTo(themaLayer.sights);
+  L.geoJSON(geojson,{
+    onEachFeature: function (feature, layer) {
+      console.log(feature);
+      console.log(`${feature.properties.NAME}`);
+      layer.bindPopup(`
+      <img src="${feature.properties.THUMBNAIL}" alt="*">
+      <h4> <a href="${feature.properties.WEITERE_INF}"
+      target="wien">${feature.properties.NAME}</a></h4>
+      <adress>${feature.properties.ADRESSE} </adress>
+      `);
+    }
+  }).addTo(themaLayer.sights);
 }
 loadSights("https://data.wien.gv.at/daten/geo?service=WFS&request=GetFeature&version=1.1.0&typeName=ogdwien:SEHENSWUERDIGOGD&srsName=EPSG:4326&outputFormat=json")
