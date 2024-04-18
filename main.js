f1afeeeec265c1eda5f0e0153cd4cabc07720bdd
madeleinehll <hill.madeleine13@yahoo.de>
2024-04-18 18:02:58 +0200

    Links angepasst

--- main.js
+++ main.js
@@ -61,7 +61,6 @@ async function loadSights(url) {
   console.log("Loading", url);
   let response = await fetch (url);
   let geojson = await response.json();
-  //console.log(geojson);
   L.geoJSON(geojson,{
     onEachFeature: function (feature, layer) {
       console.log(feature);
@@ -81,42 +80,34 @@ async function loadLines(url) {
   console.log("Loading", url);
   let response = await fetch (url);
   let geojson = await response.json();
-  //console.log(geojson);
   L.geoJSON(geojson,{
     onEachFeature: function (feature, layer) {
       console.log(feature);
-      console.log(`${feature.properties.NAME}`);
+      console.log(`${feature.properties.LINE_NAME}`);
       layer.bindPopup(`
-      <img src="${feature.properties.THUMBNAIL}" alt="*">
-      <h4> <a href="${feature.properties.WEITERE_INF}"
-      target="wien">${feature.properties.NAME}</a></h4>
-      <adress>${feature.properties.ADRESSE} </adress>
+    
       `);
     }
   }).addTo(themaLayer.lines);
 }
-loadLines("https://www.data.gv.at/katalog/de/dataset/touristische-kraftfahrlinien-liniennetz-vienna-sightseeing-linie-wien")
+loadLines("https://data.wien.gv.at/daten/geo?service=WFS&request=GetFeature&version=1.1.0&typeName=ogdwien:TOURISTIKLINIEVSLOGD&srsName=EPSG:4326&outputFormat=csv")
 
 
 async function loadStops(url) {
   console.log("Loading", url);
   let response = await fetch (url);
   let geojson = await response.json();
-  //console.log(geojson);
   L.geoJSON(geojson,{
     onEachFeature: function (feature, layer) {
       console.log(feature);
-      console.log(`${feature.properties.NAME}`);
+      console.log(`${feature.properties.STAT_NAME}`);
       layer.bindPopup(`
-      <img src="${feature.properties.THUMBNAIL}" alt="*">
-      <h4> <a href="${feature.properties.WEITERE_INF}"
-      target="wien">${feature.properties.NAME}</a></h4>
-      <adress>${feature.properties.ADRESSE} </adress>
+    
       `);
     }
   }).addTo(themaLayer.stops);
 }
-loadStops("https://www.data.gv.at/katalog/de/dataset/touristische-kraftfahrlinien-haltestellen-vienna-sightseeing-linie-standorte-wien")
+loadStops("https://data.wien.gv.at/daten/geo?service=WFS&request=GetFeature&version=1.1.0&typeName=ogdwien:TOURISTIKHTSVSLOGD&srsName=EPSG:4326&outputFormat=json")
 
 
 
@@ -124,39 +115,31 @@ async function loadZones(url) {
   console.log("Loading", url);
   let response = await fetch (url);
   let geojson = await response.json();
-  //console.log(geojson);
   L.geoJSON(geojson,{
     onEachFeature: function (feature, layer) {
       console.log(feature);
-      console.log(`${feature.properties.NAME}`);
+      console.log(`${feature.properties.ADRESSE}`);
       layer.bindPopup(`
-      <img src="${feature.properties.THUMBNAIL}" alt="*">
-      <h4> <a href="${feature.properties.WEITERE_INF}"
-      target="wien">${feature.properties.NAME}</a></h4>
-      <adress>${feature.properties.ADRESSE} </adress>
+     
       `);
     }
   }).addTo(themaLayer.zones);
 }
-loadZones("https://www.data.gv.at/katalog/de/dataset/stadt-wien_fugngerzonenwien")
+loadZones("https://data.wien.gv.at/daten/geo?service=WFS&request=GetFeature&version=1.1.0&typeName=ogdwien:FUSSGEHERZONEOGD&srsName=EPSG:4326&outputFormat=json")
 
 async function loadHotels(url) {
   console.log("Loading", url);
   let response = await fetch (url);
   let geojson = await response.json();
-  //console.log(geojson);
   L.geoJSON(geojson,{
     onEachFeature: function (feature, layer) {
       console.log(feature);
-      console.log(`${feature.properties.NAME}`);
+      console.log(`${feature.properties.BETRIEB}`);
       layer.bindPopup(`
-      <img src="${feature.properties.THUMBNAIL}" alt="*">
-      <h4> <a href="${feature.properties.WEITERE_INF}"
-      target="wien">${feature.properties.NAME}</a></h4>
       <adress>${feature.properties.ADRESSE} </adress>
       `);
     }
   }).addTo(themaLayer.hotels);
 }
-loadHotels("https://www.data.gv.at/katalog/de/dataset/hotels-und-unterkunfte-in-wien")
+loadHotels("https://data.wien.gv.at/daten/geo?service=WFS&request=GetFeature&version=1.1.0&typeName=ogdwien:UNTERKUNFTOGD&srsName=EPSG:4326&outputFormat=json")
 
